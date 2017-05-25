65b2a95df56c473ee46b021aea6bb560f651d3d8
Jan Augustyniak <janek.augustyniak@gmail.com>
2017-05-25 10:59:18 +0200

    Detect chess fields.

--- CheckersApplication/CheckersApplication/ChessField.cs
+++ CheckersApplication/CheckersApplication/ChessField.cs
@@ -12,7 +12,7 @@ namespace CheckersApplication
     public class ChessField
     {
         public Point[] points { get; } = new Point[4];
-        public int Value; 
+        public int Value;
 
         public ChessField(Point[] points)
         {
@@ -22,37 +22,19 @@ namespace CheckersApplication
 
         public static ChessField[,] GetChessFields(Point[] rectangle)
         {
-            Point[] rectangle2 = rectangle;
-            IEnumerable<Point> query = rectangle2.OrderBy(r => r.X);
-            if (query.ElementAt<Point>(0).Y < query.ElementAt<Point>(1).Y)
-            {
-                rectangle[0] = query.ElementAt<Point>(0);
-                rectangle[2] = query.ElementAt<Point>(1);
-            }
-            else
-            {
-                rectangle[0] = query.ElementAt<Point>(1);
-                rectangle[2] = query.ElementAt<Point>(0);
-            }
 
-            if (query.ElementAt<Point>(2).Y < query.ElementAt<Point>(3).Y)
-            {
-                rectangle[1] = query.ElementAt<Point>(2);
-                rectangle[3] = query.ElementAt<Point>(3);
-            }
-            else
-            {
-                rectangle[1] = query.ElementAt<Point>(3);
-                rectangle[3] = query.ElementAt<Point>(2);
-            }
+            IEnumerable<Point> queryX = rectangle.OrderBy(r => r.X);
+            IEnumerable<Point> queryY = rectangle.OrderBy(r => r.Y);
 
-            int width = rectangle[1].X - rectangle[0].X;
-            int height = rectangle[3].Y - rectangle[0].Y;
+            int width = queryX.ElementAt<Point>(3).X - queryX.ElementAt<Point>(0).X;
+            int height = queryY.ElementAt<Point>(3).Y - queryY.ElementAt<Point>(0).Y;
+            rectangle[0].X = queryX.ElementAt<Point>(0).X;
+            rectangle[0].Y = queryY.ElementAt<Point>(0).Y;
             int fieldWidht = width / 8;
             int fieldHeight = height / 8;
             Point[] field = new Point[4];
-            ChessField[,] fields = new ChessField[8,8];
-            for(int i = 0; i < 8; i++)
+            ChessField[,] fields = new ChessField[8, 8];
+            for (int i = 0; i < 8; i++)
             {
                 field[0].Y = rectangle[0].Y + i * fieldHeight;
                 field[1].Y = rectangle[0].Y + i * fieldHeight;
@@ -67,7 +49,7 @@ namespace CheckersApplication
                     fields[i, j] = new ChessField(field);
                 }
             }
-            return fields;            
+            return fields;
         }
 
         public static void Pons(ChessField[,] fields, CircleF[] circles)
@@ -76,7 +58,7 @@ namespace CheckersApplication
             {
                 foreach (ChessField field in fields)
                 {
-                    if(circle.Center.X>field.points[0].X && circle.Center.X < field.points[1].X && circle.Center.Y > field.points[0].Y && circle.Center.Y < field.points[3].Y)
+                    if (circle.Center.X > field.points[0].X && circle.Center.X < field.points[1].X && circle.Center.Y > field.points[0].Y && circle.Center.Y < field.points[3].Y)
                         field.Value = 2;
                 }
             }
